2eaa64c028c4124462a4326239b4b137cb02bd67
Anuragkar234 <msm19b021@iiitdm.ac.in>
2021-01-22 20:19:58 +0530

    Added blog

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = (
-    (0, "Draft")
+    (0, "Draft"),
     (1, "Publish")
 )
 
@@ -14,6 +14,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
